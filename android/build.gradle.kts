d7bbd0ca7e7d312ad9dd01981715f046901a8b33
Zain Ul Abideen <kingop333op@gmail.com>
2025-08-27 18:23:25 +0500

    build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,10 +3,7 @@ buildscript {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath("com.google.gms:google-services:4.3.15")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-    }
+    
 }
 
 allprojects {
@@ -17,16 +14,12 @@ allprojects {
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
 
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
+
+   
+
